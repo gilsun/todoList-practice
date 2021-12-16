736985b16b9427209d7b8d5097d5146184fe5dc1
gilsun <gilsun0821@gmail.com>
2021-12-16 00:58:14 -0800

    create inputForm and add the function for submitTodoHandler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
 import "./App.css";
+import { useState } from "react";
+//import components
+import InputForm from "./components/InputForm";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello React todo list </h1>
+      <header>
+        <h1>Gilsun's Todo List {inputText}</h1>
+      </header>
+      <InputForm todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
 
 export default App;
+ 
